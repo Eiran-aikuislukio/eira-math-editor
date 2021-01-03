a00b7334bd57427ac0f6678df73b30cbe91ecb1b
Joe Smallwood <joeharrysmallwood@gmail.com>
2021-01-03 16:56:57 +0200

    Add upload icon

---
+++ src/components/UploadIcon.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Icon } from '@chakra-ui/react'
+
+const UploadIcon = (props) => (
+  <Icon viewBox="0 0 14 14" {...props}>
+    <path
+      fill="currentColo"
+      d="M10.2857 4.37143L9.08571 5.57143L6.85714 3.28071V9.42857H5.14286V3.28071L2.91429 5.57143L1.71429 4.37143L6 0L10.2857 4.37143ZM0 10.2857V12H12V10.2857H0Z"
+    />
+  </Icon>
+)
+
+export default UploadIcon
