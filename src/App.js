68fa2c5f8091631093f575886536b8a6ead05632
Joe Smallwood <joeharrysmallwood@gmail.com>
2021-01-03 16:49:16 +0200

    Add global styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import { ChakraProvider } from '@chakra-ui/react'
+import { css, Global } from '@emotion/react'
 
 import Layout from './components/Layout'
 import theme from './theme'
 
+const globalStyles = css`
+  body {
+    margin: 0;
+  }
+`
+
 const App = () => (
   <ChakraProvider theme={theme}>
+    <Global styles={globalStyles} />
     <Layout />
   </ChakraProvider>
 )
