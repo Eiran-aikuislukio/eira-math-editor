a8e09d701b0bb7356303c26a69cd8f7a7f8bf20b
Joe Smallwood <joeharrysmallwood@gmail.com>
2021-10-02 18:15:57 +0300

    Add ga

--- src/index.js
+++ src/index.js
@@ -4,12 +4,34 @@ import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
+const injectAnalytics = () => {
+  if (!process.env.REACT_APP_GA_MEASUREMENT_ID) {
+    return
+  }
+
+  const script = document.createElement('script')
+  script.async = true
+  script.src = `https://www.googletagmanager.com/gtag/js?id=${process.env.REACT_APP_GA_MEASUREMENT_ID}`
+  document.body.appendChild(script)
+
+  window.dataLayer = window.dataLayer || []
+
+  function gtag() {
+    window.dataLayer.push(arguments)
+  }
+
+  gtag('js', new Date())
+  gtag('config', process.env.REACT_APP_GA_MEASUREMENT_ID)
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 )
+
+injectAnalytics()
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
